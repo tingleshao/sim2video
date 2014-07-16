a9948402a745d08c6a205a56c3cbc0b48ae0a4b7
Chong Shao <chongshao-mikasa@maincampusmid-v882-02963.1xwireless.unc.edu>
2014-07-16 16:02:39 -0400

    Initial Commit

---
+++ sim2video/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  sim2video
+//
+//  Created by Chong Shao on 7/16/14.
+//  Copyright (c) 2014 Chong Shao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
